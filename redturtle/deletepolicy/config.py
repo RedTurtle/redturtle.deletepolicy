525f5f2c6029752aa61144423586bfb32d980e3e
keul <luca@keul.it>
2010-12-06 13:29:38 +0000

    Moved from RedTurtle internal SVN

---
+++ redturtle/deletepolicy/config.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from Products.CMFCore.DirectoryView import registerDirectory
+GLOBALS = globals()
+
+registerDirectory('skins', GLOBALS)
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0.3'
+
+setup(name='redturtle.deletepolicy',
+      version=version,
+      description="Modified policy for deleting objects in Plone 3",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='delete policy permissions',
+      author='RedTurtle Technology',
+      author_email='info@redturtle.net',
+      url='https://code.redturtle.it/svn/redturtle/redturtle.deletepolicy/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['redturtle'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
