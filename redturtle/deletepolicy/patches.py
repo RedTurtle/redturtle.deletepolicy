04234770ed4615ff2ad191555e220dcfb49c30ed
federicadelia <federica.delia@redturtle.it>
2012-12-03 21:24:40 +0000

    Fixed low level Archetypes permissions

---
+++ redturtle/deletepolicy/patches.py
@@ -0,0 +1,43 @@
+# Monkey patch for default manage_cutObjects behaviour. 
+# We don't want to have ModifyPortalContent permission to be allow to manage_cutObjects.
+# So now we are using 'Delete objects'
+from Globals import InitializeClass
+from Products.CMFCore import permissions
+from Products.CMFPlone.PloneFolder import BasePloneFolder
+from Products.Archetypes.BaseFolder import BaseFolderMixin
+from Products.Archetypes.BaseBTreeFolder import BaseBTreeFolder
+from Products.CMFPlone.Portal import PloneSite
+from AccessControl.Permissions import delete_objects, copy_or_move, view_management_screens
+
+def _update_permissionsPaste(_class):
+    old_permissions = dict(_class.__ac_permissions__).copy()
+    try:
+        mpc = list(old_permissions[permissions.ModifyPortalContent])
+        mpc.remove('manage_pasteObjects')
+        old_permissions[permissions.ModifyPortalContent] = tuple(mpc)
+    except KeyError:
+        pass
+    _class.__ac_permissions__ = ((permissions.AddPortalContent, ('manage_pasteObjects',)),) + tuple(old_permissions.items())
+    InitializeClass(_class)
+    
+_update_permissionsPaste(BasePloneFolder)
+_update_permissionsPaste(BaseFolderMixin)
+_update_permissionsPaste(BaseBTreeFolder)
+_update_permissionsPaste(PloneSite)
+print "*** Monkeypatched default permission for paste objects (from ModifyPortalContent to AddPortalContent) ***\n"
+
+def _update_permissionsCut(_class):
+    old_permissions = dict(_class.__ac_permissions__).copy()
+    try:
+        mpc = list(old_permissions[permissions.ModifyPortalContent])
+        mpc.remove('manage_cutObjects')
+        old_permissions[permissions.ModifyPortalContent] = tuple(mpc)
+    except KeyError:
+        pass
+    _class.__ac_permissions__ = ((delete_objects, ('manage_cutObjects',)),) + tuple(old_permissions.items())
+    InitializeClass(_class)
+    
+_update_permissionsCut(BasePloneFolder)
+_update_permissionsCut(BaseFolderMixin)
+_update_permissionsCut(BaseBTreeFolder)
+print "*** Monkeypatched default permission for cut objects (from ModifyPortalContent to DeleteObject) ***\n"
